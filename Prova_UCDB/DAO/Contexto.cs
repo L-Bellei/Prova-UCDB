3e9bae74b199ae86e29cfd19f5594e3d10d0f5cd
Frebells <leo14bellei@gmail.com>
2022-02-04 22:48:01 -0400

    Add project files.

---
+++ Prova_UCDB/DAO/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Prova_UCDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Prova_UCDB.DAO
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Pedido> Pedido { get; set; }
+    }
+}
